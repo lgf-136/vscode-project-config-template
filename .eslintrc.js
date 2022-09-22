ea6e885e8e867b5097f6cc21d527fd0749218db8
lgf <lgf_136@163.com>
2022-09-23 04:47:30 +0800

    fix: 🐛 change eslint config
    change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended']
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended', //common
+    'plugin:prettier/recommended' //common
+    // "plugin:vue/vue3-essential", // vue
+    // "@vue/typescript/recommended", // vue
+    // "@vue/prettier", // vue
+    // "@vue/prettier/@typescript-eslint", // vue
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 };
